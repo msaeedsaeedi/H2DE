9be5034cf10c6e09f00ee8b08681c8e71ecd507c
Mohammad Saeed <msaeedsaeedi@outlook.com>
2024-07-16 19:48:04 +0500

    (feat): Configuration Utility
    Implemented H2DEExceptions.

--- include/H2DE/utils/readconfig.hpp
+++ include/H2DE/utils/readconfig.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <string>
 
+#include "H2DE/exceptions/io_exceptions.hpp"
 #include "libconfig.h++"
 
 namespace utils
@@ -16,18 +17,19 @@ namespace utils
 
         private:
             libconfig::Config cfg;
+            std::string config_file;
     };
 }  // namespace utils
 
-inline utils::read_config::read_config(const std::string &config_file) {
+inline utils::read_config::read_config(const std::string &config_file)
+    : config_file(config_file) {
     try {
         cfg.readFile(config_file);
     } catch (const libconfig::FileIOException &fioex) {
-        throw std::runtime_error("I/O error while reading file.");
+        throw H2DE::IOException(config_file);
     } catch (const libconfig::ParseException &pex) {
-        throw std::runtime_error("Parse error at " +
-                                 std::to_string(pex.getLine()) + ": " +
-                                 pex.getError());
+        throw H2DE::ParseException(pex.getFile(), pex.getLine(),
+                                   pex.getError());
     }
 }
 inline utils::read_config::~read_config() {
@@ -35,11 +37,16 @@ inline utils::read_config::~read_config() {
 }
 template <typename T>
 inline T utils::read_config::get(const std::string &key) {
-    T value;
     try {
-        cfg.lookupValue(key, value);
+        libconfig::Setting &setting = cfg.lookup(key);
+        return static_cast<T>(setting);
+    } catch (const libconfig::SettingTypeException &stex) {
+        throw H2DE::ParseException(
+            this->config_file,
+            "Invalid type for key { " + std::string(stex.getPath()) + " }");
     } catch (const libconfig::SettingNotFoundException &nfex) {
-        throw std::runtime_error("Setting not found: " + key);
+        throw H2DE::ParseException(
+            this->config_file,
+            "No such key { " + std::string(nfex.getPath()) + " }");
     }
-    return value;
 }
