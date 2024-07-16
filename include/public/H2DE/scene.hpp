e9c440dfdf62dd2fec3197848072a986d095daf6
Mohammad Saeed <msaeedsaeedi@outlook.com>
2024-07-16 21:04:15 +0500

    (feat): Exception Handling + Renaming
    1. Added checks for all methods.
    2. Encapsulated `Scene` in `H2DE` namespace

--- include/public/H2DE/scene.hpp
+++ include/public/H2DE/scene.hpp
@@ -1,10 +1,13 @@
 #pragma once
 
-class Scene
+namespace H2DE
 {
-    public:
-        virtual ~Scene() = default;
-        virtual void init() = 0;
-        virtual void update(const float delta) = 0;
-        virtual void render() = 0;
-};
+    class Scene
+    {
+        public:
+            virtual ~Scene() = default;
+            virtual void init() = 0;
+            virtual void update(const float delta) = 0;
+            virtual void render() = 0;
+    };
+}  // namespace H2DE
--- src/scene_manager.cpp
+++ src/scene_manager.cpp
@@ -2,36 +2,41 @@
 
 #include "H2DE/exceptions/scene_exceptions.hpp"
 
-std::unordered_map<std::string, std::shared_ptr<Scene>>
+std::unordered_map<std::string, std::shared_ptr<H2DE::Scene>>
     H2DE::SceneManager::m_scenes;
-std::shared_ptr<Scene> H2DE::SceneManager::m_current_scene;
+std::shared_ptr<H2DE::Scene> H2DE::SceneManager::m_current_scene;
 
 void H2DE::SceneManager::add_scene(const std::string& name,
-                                   const std::shared_ptr<Scene>& scene) {
+                                   const std::shared_ptr<H2DE::Scene>& scene) {
+    if (m_scenes.find(name) != m_scenes.end())
+        throw H2DE::SceneException("Scene already exists");
     m_scenes[name] = scene;
-    if (m_current_scene == nullptr) {
+    if (m_current_scene == nullptr)
         m_current_scene = scene;
-    }
 }
 
 void H2DE::SceneManager::change_scene(const std::string& name) {
+    if (m_scenes.find(name) == m_scenes.end()) {
+        throw H2DE::SceneException("Scene not found");
+    }
     m_current_scene = m_scenes[name];
 }
 
 void H2DE::SceneManager::remove_scene(const std::string& name) {
+    if (m_scenes.find(name) == m_scenes.end())
+        throw H2DE::SceneException("Scene not found");
     m_scenes.erase(name);
 }
 
-std::shared_ptr<Scene> H2DE::SceneManager::get_scene(const std::string& name) {
-    if (m_scenes.find(name) == m_scenes.end()) {
+std::shared_ptr<H2DE::Scene> H2DE::SceneManager::get_scene(
+    const std::string& name) {
+    if (m_scenes.find(name) == m_scenes.end())
         throw H2DE::SceneException("Scene not found");
-    }
     return m_scenes.at(name);
 }
 
-std::shared_ptr<Scene> H2DE::SceneManager::get_current_scene() {
-    if (m_current_scene == nullptr) {
+std::shared_ptr<H2DE::Scene> H2DE::SceneManager::get_current_scene() {
+    if (m_current_scene == nullptr)
         throw H2DE::SceneException("No scene is currently set");
-    }
     return m_current_scene;
 }
