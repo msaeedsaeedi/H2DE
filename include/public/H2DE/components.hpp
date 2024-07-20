58d5518b293037384ad0ebfcd48eac88d5db4b64
msaeedsaeedi <msaeedsaeedi@outlook.com>
2024-07-20 18:16:34 +0500

    (chore): `Sprite` Component Added
    Sprite component will be responsible for loading and rendering sprites on the screen

--- include/public/H2DE/components.hpp
+++ include/public/H2DE/components.hpp
@@ -1,7 +1,10 @@
 #pragma once
+#include <utility>
 
 namespace H2DE
 {
+    class RenderEngine;
+
     namespace Component
     {
         struct Transform {
@@ -11,6 +14,22 @@ namespace H2DE
 
                 std::pair<float, float> position;
                 bool has = false;
+        };
+
+        class Sprite
+        {
+            private:
+                friend class H2DE::RenderEngine;
+
+            public:
+                Sprite() = default;
+                Sprite(const std::string& url);
+
+                std::pair<float, float> scale{1.0f, 1.0f};
+                bool has = false;
+
+            private:
+                uint32_t asset_id;
         };
     }  // namespace Component
 }  // namespace H2DE
