77f3fe8b0e02dff9431de82cc3b10610a2d18404
msaeedsaeedi <msaeedsaeedi@outlook.com>
2024-07-21 23:00:17 +0500

    (test): Add unit tests for Config class
    This commit adds unit tests for the newly added Config class. The tests cover the initialization of the Config object, setting and getting scalar values, setting and getting array values, and verifying the correctness of the retrieved values. The addition of these tests ensures the reliability and functionality of the Config class.

---
+++ test/src/Config.cpp
@@ -0,0 +1,67 @@
+#include <gtest/gtest.h>
+
+#include <filesystem>
+
+#include "H2DE/utils/Config.hpp"
+
+class WriteConfigTest : public ::testing::Test
+{
+    protected:
+        void SetUp() override {
+            std::filesystem::create_directory("assets/configuration");
+        }
+
+        void TearDown() override {
+            std::filesystem::remove_all("assets/configuration");
+        }
+};
+
+TEST_F(WriteConfigTest, init) {
+    EXPECT_NO_THROW(H2DE::Config config("assets/configuration/config.cfg"));
+    EXPECT_NO_THROW(H2DE::Config config("assets/configuration/config.cfg"));
+}
+
+TEST_F(WriteConfigTest, scalar) {
+    H2DE::Config config("assets/configuration/config.cfg");
+    config.set("data.int", 32);
+    config.set("data.float", 23.0f);
+    config.set("data.string", "Hello, World!");
+    config.set("data.bool", true);
+
+    EXPECT_FLOAT_EQ(config.get<float>("data.float"), 23.0f);
+    EXPECT_EQ(config.get<int>("data.int"), 32);
+    EXPECT_STREQ(config.get<std::string>("data.string").c_str(),
+                 "Hello, World!");
+    EXPECT_EQ(config.get<bool>("data.bool"), true);
+}
+
+TEST_F(WriteConfigTest, array) {
+    H2DE::Config config("assets/configuration/config.cfg");
+    config.set("array.int", std::array<int, 3>{1, 2, 3});
+    config.set("array.float", std::array<float, 3>{1.0f, 2.0f, 3.0f});
+    config.set("array.string",
+               std::array<std::string, 3>{"Hello", "World", "!"});
+    config.set("array.bool", std::array<bool, 3>{true, false, true});
+
+    std::array<int, 3> data = config.get<int, 3>("array.int");
+    std::array<float, 3> data2 = config.get<float, 3>("array.float");
+    std::array<std::string, 3> data3 =
+        config.get<std::string, 3>("array.string");
+    std::array<bool, 3> data4 = config.get<bool, 3>("array.bool");
+
+    EXPECT_EQ(data[0], 1);
+    EXPECT_EQ(data[1], 2);
+    EXPECT_EQ(data[2], 3);
+
+    EXPECT_FLOAT_EQ(data2[0], 1.0f);
+    EXPECT_FLOAT_EQ(data2[1], 2.0f);
+    EXPECT_FLOAT_EQ(data2[2], 3.0f);
+
+    EXPECT_STREQ(data3[0].c_str(), "Hello");
+    EXPECT_STREQ(data3[1].c_str(), "World");
+    EXPECT_STREQ(data3[2].c_str(), "!");
+
+    EXPECT_EQ(data4[0], true);
+    EXPECT_EQ(data4[1], false);
+    EXPECT_EQ(data4[2], true);
+}
