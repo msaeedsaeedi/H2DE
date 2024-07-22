038a3860dad0ad53d97ff3bc241ad284e61c295a
msaeedsaeedi <msaeedsaeedi@outlook.com>
2024-07-22 23:14:26 +0500

    refactor: Remove unused libconfig dependency in bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -25,24 +25,12 @@ install_from_github() {
 SFML_VERSION="2.6.1"
 SFML_REPO="https://github.com/SFML/SFML.git"
 
-# Variables for libconfig
-LIBCONFIG_VERSION="1.7.3"
-LIBCONFIG_REPO="https://github.com/hyperrealm/libconfig.git"
-LIBCONFIG_PACKAGE_NAME="libconfig"
-
 # Check if SFML is installed
 if [ -d "external/SFML" ] && [ -n "$(ls external/SFML)" ]; then
     echo "SFML $SFML_VERSION is already installed."
 else
     # Install SFML 2.6.1 if not already installed
     install_from_github $SFML_REPO $SFML_VERSION "SFML" $SFML_VERSION
-fi
-
-# Install the latest version of libconfig if not already installed
-if [ -d "external/libconfig" ] && [ -n "$(ls external/libconfig)" ]; then
-    echo "libconfig $LIBCONFIG_VERSION is already installed."
-else
-    install_from_github $LIBCONFIG_REPO "master" $LIBCONFIG_PACKAGE_NAME $LIBCONFIG_VERSION
 fi
 
 echo "All installations completed successfully."
