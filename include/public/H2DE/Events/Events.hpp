a16aa07ac31e27283deeae3aef0f34b1ace3badd
msaeedsaeedi <msaeedsaeedi@outlook.com>
2024-07-23 21:22:17 +0500

    💡 Add Doxygen comments to `Event's enums` for better API documentation
    Enums:
    1. WindowEventType
    2. KeyState
    3. MouseEventType
    4. Key

--- include/public/H2DE/Events/Events.hpp
+++ include/public/H2DE/Events/Events.hpp
@@ -2,23 +2,44 @@
 
 namespace H2DE
 {
+    /**
+     * @enum WindowEventType
+     * @brief Enum class for window event types.
+     *
+     * This enum class defines various types of window events, such as closing,
+     * resizing, gaining or losing focus, and mouse events like entering or
+     * leaving the window.
+     */
     enum class WindowEventType {
-        WINDOW_CLOSE,
-        WINDOW_RESIZE,
-        WINDOW_LOST_FOCUS,
-        WINDOW_GAINED_FOCUS,
-        MOUSE_ENTERED = 12,
-        MOUSE_LEFT = 13,
+        WINDOW_CLOSE,        /**< Event type for window close */
+        WINDOW_RESIZE,       /**< Event type for window resize */
+        WINDOW_LOST_FOCUS,   /**< Event type for window losing focus */
+        WINDOW_GAINED_FOCUS, /**< Event type for window gaining focus */
+        MOUSE_ENTERED = 12,  /**< Event type for mouse entering the window */
+        MOUSE_LEFT = 13,     /**< Event type for mouse leaving the window */
     };
 
+    /**
+     * @enum KeyState
+     * @brief Enum class for key states.
+     *
+     * This enum class defines the states of a key, such as pressed or released.
+     */
     enum class KeyState {
-        Pressed,
-        Released,
+        Pressed,  /**< Key state for when a key is pressed */
+        Released, /**< Key state for when a key is released */
     };
 
+    /**
+     * @enum MouseEventType
+     * @brief Enum class for mouse event types.
+     *
+     * This enum class defines various types of mouse events, such as scrolling,
+     * mouse button actions, and mouse movement.
+     */
     enum class MouseEventType {
-        MOUSE_WHEEL_SCROLLED = 8,
-        MOUSE_KEY = 9,
-        MOUSE_MOVED = 11,
+        MOUSE_WHEEL_SCROLLED = 8, /**< Event type for mouse wheel scrolled */
+        MOUSE_KEY = 9,            /**< Event type for mouse button actions */
+        MOUSE_MOVED = 11,         /**< Event type for mouse movement */
     };
 };  // namespace H2DE
