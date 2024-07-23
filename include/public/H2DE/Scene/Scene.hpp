1c908dc59171c5278888c98fdf02fe65fc4c12cc
msaeedsaeedi <msaeedsaeedi@outlook.com>
2024-07-23 21:37:04 +0500

    💡 Add Doxygen comments to `Scene Class` for better API documentation

--- include/public/H2DE/Scene/Scene.hpp
+++ include/public/H2DE/Scene/Scene.hpp
@@ -6,22 +6,57 @@
 
 namespace H2DE
 {
+    /**
+     * @class Scene
+     * @brief Represents a game scene containing objects and managing their lifecycle.
+     *
+     * The Scene class is an abstract base class for all game scenes. It manages the objects
+     * within the scene, provides methods for initialization and updates, and keeps track
+     * of the time delta between frames.
+     */
     class Scene
     {
         private:
-            float m_delta = 0.0f;
+            float m_delta = 0.0f; /**< Time delta between frames */
 
         public:
             friend class RenderEngine;
             friend class Engine;
 
+            /**
+             * @brief Default constructor.
+             */
             Scene();
+
+            /**
+             * @brief Virtual destructor.
+             */
             virtual ~Scene() = default;
+
+            /**
+             * @brief Initializes the scene.
+             *
+             * This pure virtual method must be implemented by derived classes to initialize
+             * the scene, including loading resources, setting up objects, and declaring input callbacks.
+             */
             virtual void init() = 0;
+
+            /**
+             * @brief Updates the scene.
+             *
+             * This pure virtual method must be implemented by derived classes to update the
+             * scene logic and updating object states.
+             */
             virtual void update() = 0;
+
+            /**
+             * @brief Retrieves the time delta between frames.
+             *
+             * @return The time delta between frames in seconds.
+             */
             const float get_delta() const noexcept;
 
         protected:
-            std::unique_ptr<ObjectManager> m_object_manager;
+            std::unique_ptr<ObjectManager> m_object_manager; /**< Unique pointer to the object manager */
     };
 }  // namespace H2DE
